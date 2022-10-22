657af1c297d67a44db99535208a814dc4b34cdda
nathanielg95 <nathanielg95@penguin>
2022-10-22 13:40:44 -0700

    add react website s3 bucket resources

--- lib/spotify_lamplighter-stack.ts
+++ lib/spotify_lamplighter-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class SpotifyLamplighterStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
 
-    // The code that defines your stack goes here
+      const websiteBucket = new Bucket(this, 'frontendBucket', {
+	  websiteIndexDocument: 'index.html',
+	  publicReadAccess: true
+      });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SpotifyLamplighterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      new BucketDeployment(this, 'websiteDeploy', {
+	  sources: [Source.asset(path.join(__dirname, '..', 'frontend', 'build'))],
+	  destinationBucket: websiteBucket
+      });
+
+      new cdk.CfnOutput(this, 'bucketExport', {
+	  value: websiteBucket.bucketWebsiteUrl,
+	  exportName: 'websiteBucketName'
+      });
   }
 }
