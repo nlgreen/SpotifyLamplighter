95b9e2e10adecdeee4c608821bdf767c2b1d2edb
nathanielg95 <nathanielg95@penguin>
2022-10-23 16:39:19 -0700

    add user pool, app client, and authorization flow to api gateway

--- lib/spotify_lamplighter-stack.ts
+++ lib/spotify_lamplighter-stack.ts
@@ -5,6 +5,9 @@ import { Bucket } from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { HttpApi, CorsHttpMethod, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { HttpJwtAuthorizer } from '@aws-cdk/aws-apigatewayv2-authorizers-alpha';
+import { UserPool } from 'aws-cdk-lib/aws-cognito';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 
 export class SpotifyLamplighterStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -35,11 +38,40 @@ export class SpotifyLamplighterStack extends cdk.Stack {
 	    createDefaultStage: true
 	});
 
+
+	const userPool = new UserPool(this, 'UserPool', {
+	    selfSignUpEnabled: false,
+	});
+
+	userPool.addDomain('UserPoolDomain', {
+	    cognitoDomain: {
+		domainPrefix: 'spotify-lamplighter'
+	    }
+	});
+	
+	const userPoolAppClient = userPool.addClient('UserPoolClient', {
+	    oAuth: {
+		callbackUrls: ["https://example.com/callback"],
+		logoutUrls: ["https://example.com/logout"]
+	    },
+	    userPoolClientName: 'SpotifyLamplighterAppClient'
+	});
+
+	const issuer = 'https://cognito-idp.us-east-1.amazonaws.com/' + userPool.userPoolId;
+	const identitySource = '$request.header.Authorization';
+
+	const authorizer = new HttpJwtAuthorizer('APIAuthorizer', issuer, {
+	    jwtAudience: [userPoolAppClient.userPoolClientId],
+	    authorizerName: userPoolAppClient.userPoolClientName,
+	    identitySource: [identitySource]
+	});
+
 	const lambdaIntegration = new HttpLambdaIntegration('APIIntegration', apiLambda);
 	httpApi.addRoutes({
 	    path: '/getSongTable',
 	    methods: [ HttpMethod.GET ],
-	    integration: lambdaIntegration
+	    integration: lambdaIntegration,
+	    authorizer
 	});
 
 	new cdk.CfnOutput(this, 'apiExport', {
